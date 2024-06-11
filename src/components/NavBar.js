0bc9ae894e062b1ac3a6eefe4782c7b1e41425e6
GigiLi89 <li.gigi89@gmail.com>
2024-06-11 08:19:32 +0000

    Added styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo_memo.png";
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
   return (
-        <Navbar expand="md" fixed="top">
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="45" />
@@ -13,9 +14,9 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link><i className="fas fa-home"></i> Home</Nav.Link>
-            <Nav.Link><i className="fas fa-sign-in-alt"></i> Sign in</Nav.Link>
-            <Nav.Link><i className="fas fa-user-plus"></i> Sign up</Nav.Link>
+            <Nav.Link><i className="fas fa-home"></i> HOME</Nav.Link>
+            <Nav.Link><i className="fas fa-sign-in-alt"></i> SIGN IN</Nav.Link>
+            <Nav.Link><i className="fas fa-user-plus"></i> SIGN UP</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
